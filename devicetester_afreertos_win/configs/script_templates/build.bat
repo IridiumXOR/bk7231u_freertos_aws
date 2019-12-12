ce8032e1bf68540ca7e3ddc99fa6578795bca353
heng.zhang <heng.zhang@bekencorp.com>
2019-12-12 15:06:43 +0800

    change repository struct like github.com/aws/amazon-freertos

--- devicetester_afreertos_win/configs/script_templates/build.bat
+++
@@ -1,8 +0,0 @@
-REM #!/bin/bash
-REM # usage: script_templates/build.bat ablosute_path_to_build_path
-
-cd /d %1
-rmdir /s /q build
-cmake -DVENDOR=beken -DBOARD=bk7231u -DCOMPILER=arm-gcc -DAFR_ENABLE_TESTS=1 -S. -Bbuild -G"Unix Makefiles"
-cd build
-make -j8
--- devicetester_afreertos_win/configs/script_templates/flash.bat
+++
@@ -1,7 +0,0 @@
-REM #!/bin/bash
-
-cd /d %1
-
-start /wait projects\beken\tools\bk_writer_V1.52_20190909.exe -tBK7231 -p%2@1000000 -fbuild\vendors\beken\boards\bk7231u\bk7231u_uart_0.0.1.bin@00011000 -e2
-
-echo bk_writer return %errorlevel%
